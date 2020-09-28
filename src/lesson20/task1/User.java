6300248d0014875b3d89cf0bc791989a5fcbb8bc
Bdvoga <samib@mail.ru>
2020-09-28 20:29:18 +0300

    Урок 20 - Задача с урока

--- src/lesson20/task1/User.java
+++ src/lesson20/task1/User.java
@@ -15,23 +15,11 @@ public class User {
         return id;
     }
 
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getSessionId() {
         return sessionId;
-    }
-
-    public void setSessionId(String sessionId) {
-        this.sessionId = sessionId;
     }
 }
--- src/lesson20/task1/UserRepository.java
+++ src/lesson20/task1/UserRepository.java
@@ -1,7 +1,11 @@
 package lesson20.task1;
 
+import lesson20.task1.exception.BadRequestException;
+import lesson20.task1.exception.InternalServelException;
+import lesson20.task1.exception.UserNotFoundException;
+
 public class UserRepository {
-    private User[] users;
+    private User[] users = new User[10];
 
     public UserRepository(User[] users) {
         this.users = users;
@@ -16,9 +20,17 @@ public class UserRepository {
     }
 
     // save
-    public User save(User user) {
-        if (findById(user.getId()) != null) {
-            return null;
+    public User save(User user) throws Exception {
+        if (user == null)
+            throw new BadRequestException("Can't save null user");
+
+        try {
+            findById(user.getId());
+            throw new BadRequestException("User with id: " + user.getId() + " already exist");
+        } catch (UserNotFoundException e) {
+            // Правило хорошего тона!
+            // Не оставлять блок catch пустым
+            System.out.println("User with id: " + user.getId() + " not found. Will be saved");
         }
 
         for (int i = 0; i < getUsers().length; i++) {
@@ -27,32 +39,32 @@ public class UserRepository {
                 return getUsers()[i];
             }
         }
-        return null;
+        throw new InternalServelException("Not enough space to save user with id: " + user.getId());
     }
 
     // update
-    public User update(User user) {
-        if (findById(user.getId()) == null) {
-            return null;
-        }
+    public User update(User user) throws Exception {
+        if (user == null)
+            throw new BadRequestException("Can't update null user");
+
+        findById(user.getId());
 
         // если есть, обновляем и возвращаем.
         for (int i =0; i < getUsers().length; i++) {
             if (getUsers()[i].getId() == user.getId()) {
                 getUsers()[i] = user;
-
                 return getUsers()[i];
             }
         }
-        return null;
+        throw new InternalServelException("Unexpected error");
     }
 
     // delete
-    public void delete(long id) {
+    public void delete(long id) throws Exception {
+
+        findById(id);
+
         for (int i = 0; i < getUsers().length; i++) {
-            if (getUsers()[i] == null) {
-                break;
-            }
             if (getUsers()[i].getId() == id) {
                 getUsers()[i] = null;
                 return;
@@ -60,14 +72,13 @@ public class UserRepository {
         }
     }
 
-    public User findById(long id) {
-        for (User el : users) {
-            if (el == null) {
-                return null;
+    public User findById(long id) throws Exception {
+        for (User user : users) {
+            if (user != null && id == user.getId()) {
+                return user;
             }
-            if (el.getId() == id)
-                return el;
         }
-        return null;
+
+        throw new UserNotFoundException("User with id: " + id + " not found");
     }
 }
