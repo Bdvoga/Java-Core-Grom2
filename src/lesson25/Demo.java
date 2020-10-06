464af0142972b28b7484a5eedf95a38e30321d96
Bdvoga <samib@mail.ru>
2020-10-06 19:12:48 +0300

    Урок 25 - Задача с урока

---
+++ src/lesson25/Demo.java
@@ -0,0 +1,28 @@
+package lesson25;
+
+public class Demo {
+
+    public static void main(String[] args) throws Exception {
+        GeneralDAO generalDAO = new GeneralDAO();
+
+        Order order = new Order(2);
+        generalDAO.validate(order);
+
+        TestClass<String, Order, Long> testClass = new TestClass<>();
+        System.out.println(testClass.doSomething("rrr"));
+
+        // Классы обертки
+        //long - Long
+        //int - Int
+        //short - Short
+
+        long variable = 111;
+
+        Long variable2 = new Long(222);
+
+        generalDAO.validate(variable2);
+
+        generalDAO.validate(variable); // автоматическое приведение к типу объекта
+
+    }
+}
