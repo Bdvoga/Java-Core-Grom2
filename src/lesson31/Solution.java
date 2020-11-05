879ca4811fc184ce216850dea6d20d6bc0ee3472
Bdvoga <samib@mail.ru>
2020-11-05 19:42:59 +0200

    Урок 31 - доработка задачи домой

--- src/lesson31/Solution.java
+++ src/lesson31/Solution.java
@@ -1,6 +1,5 @@
 package lesson31;
 
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class Solution {
@@ -9,20 +8,13 @@ public class Solution {
         HashMap<Character, Integer> map = new HashMap<>();
         char[] chars = text.toCharArray();
 
-        System.out.println(text);
-
         // буква - ключ, кол-во повторений - значение
         for (Character ch : chars) {
             if (!Character.isLetter(ch)) {
                 continue;
             }
 
-            if (map.get(ch) == null) {
-                map.put(ch, 1);
-            } else {
-                int count = map.get(ch) + 1;
-                map.put(ch, count);
-            }
+            checkChar(map, ch);
         }
 
         return map;
@@ -32,22 +24,33 @@ public class Solution {
         HashMap<String, Integer> map = new HashMap<>();
         String[] strings = text.split(" ");
 
-        System.out.println(Arrays.toString(strings));
-
         // Слово - ключ, кол-во повторений - значение
         for (String str : strings) { // Перебор слов
             if (str.length() <= 2 || !str.matches("[a-zA-Z]+")) {
                 continue;
             }
 
-            if (map.get(str) == null) {
-                map.put(str, 1);
-            } else {
-                int count = map.get(str) + 1;
-                map.put(str, count);
-            }
+            checkString(map, str);
         }
 
         return map;
+    }
+
+    private static void checkChar(HashMap<Character, Integer> map, Character ch) {
+        if (map.get(ch) == null) {
+            map.put(ch, 1);
+        } else {
+            int count = map.get(ch) + 1;
+            map.put(ch, count);
+        }
+    }
+
+    private static void checkString(HashMap<String, Integer> map, String str) {
+        if (map.get(str) == null) {
+            map.put(str, 1);
+        } else {
+            int count = map.get(str) + 1;
+            map.put(str, count);
+        }
     }
 }
