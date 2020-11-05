e18fd2bb82f5fc82f657df28ecb5c6b0f2c40c2d
Bdvoga <samib@mail.ru>
2020-11-05 21:00:21 +0200

    Урок 32 - доработка задачи домой

--- src/lesson32/ex2/Demo.java
+++ src/lesson32/ex2/Demo.java
@@ -5,6 +5,6 @@ import java.io.IOException;
 public class Demo {
 
     public static void main(String[] args) throws IOException {
-        System.out.println(Solution.readNumbers());
+        Solution.readNumbers();
     }
 }
--- src/lesson32/ex2/Solution.java
+++ src/lesson32/ex2/Solution.java
@@ -6,44 +6,45 @@ import java.io.InputStreamReader;
 
 public class Solution {
 
-    public static int readNumbers() throws IOException {
+    public static void readNumbers() throws IOException {
         int n = 3;
-        int count, sum;
+        BufferedReader br;
+        InputStreamReader reader;
+
         while (n > 0) {
             n--;
-            InputStreamReader reader = new InputStreamReader(System.in);
-            BufferedReader br = new BufferedReader(reader);
+            reader = new InputStreamReader(System.in);
+            br = new BufferedReader(reader);
             String[] str = br.readLine().split(" ");
 
             if (str.length != 10) {
-                System.out.println("Your numbers are wrong. You have " + n + " attempts to try again");
+                System.err.println("Your numbers are wrong. You have " + n + " attempts to try again");
                 continue;
             }
 
-            sum = 0;
-            count = 0;
-            for (String el : str) {
-                try {
-                    int i = new Integer(el);
-                    if (i > 100) {
-                        System.out.println("Your numbers are wrong. You have " + n + " attempts to try again");
-                        break;
-                    }
-                    count++;
-                    sum += i;
-                } catch (NumberFormatException e) {
-                    System.out.println("Your numbers are wrong. You have " + n + " attempts to try again");
-                    break;
-                }
-            }
-
-            if (count == 10) {
-                System.out.println("Amount is " + sum);
-                return sum;
-            }
+            countSum(str, n);
         }
 
         System.out.println("Your numbers are wrong. Number of attempts exceeded");
-        return 0;
+    }
+
+    private static void countSum(String[] str, int n) {
+        int sum = 0;
+        for (String el : str) {
+            try {
+                int i = new Integer(el);
+                if (i > 100) {
+                    System.err.println("Your numbers are wrong. You have " + n + " attempts to try again");
+                    return;
+                }
+                sum += i;
+            } catch (NumberFormatException e) {
+                System.err.println("Int " + "Your numbers are wrong. You have " + n + " attempts to try again");
+                return;
+            }
+        }
+
+        System.out.println("Amount is " + sum);
+        System.exit(0);
     }
 }
