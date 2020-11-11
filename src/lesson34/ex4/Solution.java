fcd617549cf73d6de4eb61cc91943057c0147a74
Bdvoga <samib@mail.ru>
2020-11-11 18:57:14 +0200

    Урок 34 - доработка задача домой 3

--- src/lesson34/ex4/Solution.java
+++ src/lesson34/ex4/Solution.java
@@ -4,7 +4,6 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 
 public class Solution {
@@ -15,16 +14,13 @@ public class Solution {
         File fileTo = new File(fileToPath);
         try {
             Files.copy(fileFrom.toPath(), fileTo.toPath());
-        } catch (FileAlreadyExistsException e) {
+        } catch (IOException e) {
             System.err.println("File " + fileToPath + " already exist");
         }
     }
 
     public static void copyFileContentApacheIO(String fileFromPath, String fileToPath) throws IOException {
 
-        File fileFrom = new File(fileFromPath);
-        File fileTo = new File(fileToPath);
-        FileUtils.copyFile(fileFrom, fileTo);
-
+        FileUtils.copyFile(new File(fileFromPath), new File(fileToPath));
     }
 }
